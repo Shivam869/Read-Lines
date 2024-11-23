fa55bf6529b88f5171c8646250cd6bea96582d72
Shivam869 <sanjivshivam23@gmail.com>
2024-11-23 21:51:11 +0530

    Add files via upload

---
+++ Read Lines.py
@@ -0,0 +1,12 @@
+file=open("Codingal.txt","r")
+print(file.readline())
+file.close()
+file=open("Codingal.txt","r")
+print(file.readline())
+print(file.readline())
+print(file.readline())
+file.close()
+file=open("Codingal.txt","r")
+for line in file:
+    print(line)
+file.close()
